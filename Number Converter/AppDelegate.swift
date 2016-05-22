6b4323bc03d94bded8b3a91039e31def06333324
Μιχαλης Παπαδοπουλος <michael_papado@yahoo.gr>
2016-05-22 09:44:57 +0300

    Initial Commit

---
+++ Number Converter/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Number Converter
+//
+//  Created by Michael Papadopoulos on 22/5/16.
+//  Copyright © 2016 Razzmatazz. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Number Converter/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Number Converter
+//
+//  Created by Michael Papadopoulos on 22/5/16.
+//  Copyright © 2016 Razzmatazz. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
